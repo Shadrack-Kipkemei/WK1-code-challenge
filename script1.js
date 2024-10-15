96c456dfa64502485ba9e03fa1325cfea1bff6d3
sktanui <shadrackkemei2019@gmail.com>
2024-10-15 15:45:10 +0300

    complete

--- script1.js
+++ script1.js
@@ -5,17 +5,18 @@ if (grade >= 0 && grade <= 100) {
     switch (true) {
         case (grade > 79):
             alert("Your grade is A");
-            
+            break
         case (grade >= 60):
             alert("Your grade is B");
-            
+            break
         case (grade >= 49):
             alert("Your grade is C");
-            
+            break
         case (grade >= 40):
             alert("Your grade is D");
-            
+            break
         default:
             alert("Your grade is E");
+            
     }
 }
